c861d4291078b465a9f82e4f59afd211f7f49920
Max Czapanskiy <47009796+FlukeAndFeather@users.noreply.github.com>
2026-02-13 22:21:04 -0800

    Add 20-year animated latitudinal profiles

--- scripts/01_download_data.py
+++ scripts/01_download_data.py
@@ -166,8 +166,8 @@ def main():
     ca_current_extent = box(minx, miny, maxx, maxy)
     download_copernicus_bgc_data(
         output_dir=copernicus_dir,
-        start_date="2024-06-01",
-        end_date="2024-06-01",
+        start_date="2005-01-01",
+        end_date="2024-12-31",
         ext=ca_current_extent,
         variables=["zooc", "npp"]
     )
